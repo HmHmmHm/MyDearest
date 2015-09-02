9e4ef4146c937e5229eee3fe874e45639a7fd3b0
hm <hmkuak@naver.com>
2015-09-02 19:55:36 +0900

    MyDearest 1.1 UPDATE, 1.5 compatible

--- src/ifteam/MyDearest/MyDearest.php
+++ src/ifteam/MyDearest/MyDearest.php
@@ -7,14 +7,19 @@ use pocketmine\event\Listener;
 use pocketmine\scheduler\CallbackTask;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
+use pocketmine\level\Level;
 
 class MyDearest extends PluginBase implements Listener {
 	public $resetTimer;
 	public function onEnable() {
 		@mkdir ( $this->getDataFolder () );
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
-		$this->resetTimer = (new Config ( $this->getDataFolder () . "settings.yml", Config::YAML, [ "resetCycle" => 1800,"message-1" => "[안내] 서버가 10초뒤 5~10초간 재부팅됩니다 *자동재부팅*","message-2" => "[안내] 서버가 재부팅됩니다.." ] ))->getAll ();
-		$this->getServer ()->getScheduler ()->scheduleDelayedTask ( new CallbackTask ( [ $this,"Reset" ] ), $this->resetTimer ["resetCycle"] * 20 );
+		$this->resetTimer = (new Config ( $this->getDataFolder () . "settings.yml", Config::YAML, [ 
+				"resetCycle" => 1800,
+				"message-1" => "[안내] 서버가 10초뒤 5~10초간 재부팅됩니다 *자동재부팅*",
+				"message-2" => "[안내] 서버가 재부팅됩니다.." 
+		] ))->getAll ();
+		$this->getServer ()->getScheduler ()->scheduleDelayedTask ( new MyDearestTask ( $this, "reset" ), $this->resetTimer ["resetCycle"] * 20 );
 	}
 	/**
 	 *
@@ -22,7 +27,7 @@ class MyDearest extends PluginBase implements Listener {
 	 */
 	public function Reset() {
 		$this->getServer ()->broadcastMessage ( TextFormat::DARK_PURPLE . $this->resetTimer ["message-1"] );
-		$this->getServer ()->getScheduler ()->scheduleDelayedTask ( new CallbackTask ( [ $this,"Shutdown" ] ), 20 * 10 );
+		$this->getServer ()->getScheduler ()->scheduleDelayedTask ( new MyDearestTask ( $this, "shutdown" ), 20 * 10 );
 	}
 	/**
 	 *
@@ -30,23 +35,38 @@ class MyDearest extends PluginBase implements Listener {
 	 */
 	public function Shutdown() {
 		$this->getServer ()->broadcastMessage ( TextFormat::DARK_PURPLE . $this->resetTimer ["message-2"] );
-		/**
-		 *
-		 * @var entitiesSave
-		 */
 		foreach ( $this->getServer ()->getOnlinePlayers () as $player ) {
 			$player->save ();
-		}
-		foreach ( $this->getServer ()->getLevels () as $level ) {
-			$level->save ( \true );
 		}
 		foreach ( $this->getServer ()->getOnlinePlayers () as $player ) {
 			$player->kick ( "서버가 곧 재부팅됩니다" );
 		}
+		foreach ( $this->getServer ()->getPluginManager ()->getPlugins () as $plugin ) {
+			if ($plugin->getName () != "MyDearest")
+				if ($plugin->isEnabled ())
+					$this->getServer ()->getPluginManager ()->disablePlugin ( $plugin );
+		}
+		foreach ( $this->getServer ()->getLevels () as $level )
+			if ($level instanceof Level)
+				$level->unload ();
 		$this->getServer ()->shutdown ();
 	}
-	public function __destruct() {
-		passthru ( \pocketmine\DATA . "MyDearest.cmd" );
+	public function onDisable() {
+		foreach ( $this->getServer ()->getOnlinePlayers () as $player ) {
+			$player->save ();
+		}
+		foreach ( $this->getServer ()->getOnlinePlayers () as $player ) {
+			$player->kick ( "서버가 곧 재부팅됩니다" );
+		}
+		foreach ( $this->getServer ()->getPluginManager ()->getPlugins () as $plugin ) {
+			if ($plugin->getName () != "MyDearest")
+				if ($plugin->isEnabled ())
+					$this->getServer ()->getPluginManager ()->disablePlugin ( $plugin );
+		}
+		foreach ( $this->getServer ()->getLevels () as $level )
+			if ($level instanceof Level)
+				$level->unload ();
+		passthru (\pocketmine\DATA . "MyDearest.cmd" );
 	}
 }
 
---
+++ src/ifteam/MyDearest/MyDearestTask.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ifteam\MyDearest;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\plugin\Plugin;
+
+class MyDearestTask extends PluginTask {
+	public $type;
+	public function __construct(Plugin $owner, $type) {
+		parent::__construct ( $owner );
+		$this->type = $type;
+	}
+	public function onRun($currentTick) {
+		switch ($type) {
+			case "reset" :
+				$this->getOwner ()->Reset ();
+				break;
+			case "shutdown" :
+				$this->getOwner ()->Shutdown ();
+				break;
+		}
+	}
+}
+
+?>
